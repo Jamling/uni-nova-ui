ed3ef5ebeac89f5891186d9a8923be4f86c53b2e
Jamling <li.jamling@gmail.com>
2019-12-27 18:05:59 +0800

    add request

---
+++ common/utils.js
@@ -0,0 +1,80 @@
+const utils = {
+    /**
+     * 对象转url参数
+     * @param {*} data
+     * @param {*} isPrefix
+     */
+    queryParams(data, isPrefix = false) {
+        let prefix = isPrefix ? '?' : ''
+        let _result = []
+        for (let key in data) {
+            let value = data[key]
+            // 去掉为空的参数
+            if (['', undefined, null].includes(value)) {
+                //continue
+            }
+            if (value.constructor === Array) {
+                value.forEach(_value => {
+                    _result.push(encodeURIComponent(key) + '[]=' + encodeURIComponent(_value))
+                })
+            } else {
+                _result.push(encodeURIComponent(key) + '=' + encodeURIComponent(value))
+            }
+        }
+
+        return _result.length ? prefix + _result.join('&') : ''
+    },
+
+    // ....其他函数....
+    formatTime(date = new Date, fmt = 'YYYY-MM-dd HH:mm:ss') {
+        //date = date || new Date
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+        const hour = date.getHours()
+        const minute = date.getMinutes()
+        const second = date.getSeconds()
+
+        var o = {
+            "M+": date.getMonth() + 1, //月份
+            "d+": date.getDate(), //日
+            "H+": date.getHours(), //小时
+            "m+": date.getMinutes(), //分
+            "s+": date.getSeconds(), //秒
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
+        return fmt;
+    },
+    // 计算距离，参数分别为第一点的纬度，经度；第二点的纬度，经度 
+    getDistance(lat1, lng1, lat2, lng2) {
+        var radLat1 = lat1 * Math.PI / 180.0;
+        var radLat2 = lat2 * Math.PI / 180.0;
+        var a = radLat1 - radLat2;
+        var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+        var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+            Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+        s = s * 6378.137; // EARTH_RADIUS 赤道半径
+        s = Math.round(s * 10000) / 10000; //  输出为KM
+        // s = s.toFixed(4); // 保留4位小数点
+        return s;
+    }
+}
+Array.prototype.indexOf = function(val) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == val) return i;
+    }
+    return -1;
+};
+Array.prototype.remove = function(val) {
+    var index = this.indexOf(val);
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+};
+
+export default utils
--- main.js
+++ main.js
@@ -1,7 +1,88 @@
 import Vue from 'vue'
 import App from './App'
+//接口文件
+import request from './common/j-request/request.js'
+import utils from './common/utils.js'
+Vue.prototype.$utils = utils
+console.log(request);
+var baseUrl = '';// 线上接口地址
+// #ifdef H5
+baseUrl = '/api'
+baseUrl = '/test';//测试接口地址
+// #endif
+request.setConfig({
+    baseUrl: baseUrl,
+    contentType: 'form',
+    debug: true
+})
+// 请求拦截
+request.interceptor.request = (config => {
+    // 给data添加全局请求参数uid
+    if (config.data.hasOwnProperty('loadindTip')) {
+        config.loadingTip = config.data.loadingTip
+        delete config.data.loadingTip
+    }
+    if (config.data.hasOwnProperty('business')) {
+        config.business = config.data.business
+        delete config.data.business
+    }
+    // 给header添加全局请求参数token
+    if (!config.header.Authorization) {
+        config.header.Authorization = global.token
+    }
+    // 添加一个自定义的参数，默认异常请求都弹出一个toast提示
+    if (config.toastError === undefined) {
+        config.toastError = true
+    }
+    return config;
+})
+// 全局的业务拦截
+request.interceptor.response = ((res, config) => {
+    if (res.code === '10001' || res.status === '10001') {
+        res.success = true;
+    } else if (res.code === 1001) {
+        // token失效，需要重新登录
+        uni.navigateTo({
+            url: '/pages/loign/login'
+        })
+    }
+    return res;
+})
 
+// 全局的错误异常处理
+request.interceptor.fail = ((res, config) => {
+    let ret = res;
+    let msg = ''
+    if (res.statusCode === 200) { // 业务错误
+        msg = res.data.msg
+        ret = res.data
+    } else if (res.statusCode > 0) { // HTTP错误
+        msg = '服务器异常[' + res.statusCode + ']'
+    } else { // 其它错误
+        msg = res.errMsg
+    }
+    if (config.toastError) {
+        uni.showToast({
+            title: msg,
+            duration: 2000,
+            icon: 'none'
+        })
+    }
+    return ret;
+})
 Vue.config.productionTip = false
+
+global.devEnv = process.env.NODE_ENV === 'development'
+global.request = request
+Vue.prototype.$global = global
+Vue.prototype.$request = request
+Vue.prototype.$toast = function(msg) {
+    uni.showToast({
+        title: msg,
+        duration: 2000,
+        icon: 'none'
+    })
+}
 
 App.mpType = 'app'
 
